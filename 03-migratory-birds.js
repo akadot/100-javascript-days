5b0c6abd8a77b8520f7f1e6a74b6d132cf494d22
Murilo Oliveira <51198449+akadot@users.noreply.github.com>
2021-11-29 15:05:33 -0300

    Update 03-migratory-birds.js

--- 03-migratory-birds.js
+++ 03-migratory-birds.js
@@ -4,16 +4,19 @@ Link - https://www.hackerrank.com/challenges/migratory-birds/problem?isFullScree
 
 function migratoryBirds(arr) {
     
-    //Using .filter to find duplicates
+    let hasDoubles = true
+    
     let duplicatesFunc = list => list.filter(
-        //If the index of first occurrence is not equal than the actual index, means the value is repeated
         (item,index)=>list.indexOf(item) !== index);
     
-    //Repeating the function 2 times to get the most repeated values
-    let mostDuplicates = duplicatesFunc(duplicatesFunc(arr))
+    let hasDoubleFunc = list => list.some((val, i) => arr.indexOf(val) !== i)
     
-    //Using Math.min to find the smallest value (the rest operator extract values because Math.min only accepts numbers)
-    let lowestValue = Math.min(...mostDuplicates)
+    while (hasDoubles === true){
+        arr = duplicatesFunc(arr);
+        hasDoubles = hasDoubleFunc(arr);
+    }    
+    
+    let lowestValue = Math.min(...arr)
     
     return lowestValue
 }
